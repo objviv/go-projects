b4c65024b5b2bd36f74c9714d2e77fb91506de94
Vivek Khanna <objviv@gmail.com>
2020-07-05 20:00:42 -0700

    Learnings/exercises.

---
+++ learnings/exercise-slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	aa := make([][]uint8, dx, dy)
+	for y := 0; y < dy; y++ {
+		a := make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			a[x] = uint8(x * y)
+		}
+		aa[y] = a
+	}
+	return aa
+}
+
+func main() {
+	pic.Show(Pic)
+}
